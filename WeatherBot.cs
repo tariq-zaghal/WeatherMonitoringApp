2840d2d19f2f91387b10cfeb930eb8f25ce7f6dd
Tariq Zaghal <zaghaltariq007@gmail.com>
2023-09-19 15:17:46 +0300

    FEAT: Create abstract class for WeatherBots

---
+++ WeatherBot.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherMonitoringApp
+{
+    public abstract class WeatherBot
+    {
+        protected bool _enabled { get; init; }
+        protected string _message { get; init; }
+
+        public WeatherBot(bool enabled, string message)
+        {
+            _enabled = enabled;
+            _message = message;
+        }
+
+        public abstract void GetInfo(WeatherData weatherData);
+    }
+}
