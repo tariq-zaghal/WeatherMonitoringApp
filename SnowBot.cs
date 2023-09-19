de4b7c0a28401b3fbcf337a7cdff0aa233976145
Tariq Zaghal <zaghaltariq007@gmail.com>
2023-09-19 16:26:40 +0300

    FEAT: Create SnowBot which gets avctivated when temperature goes below certain threshold

---
+++ SnowBot.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherMonitoringApp
+{
+    public class SnowBot : WeatherBot
+    {
+        private decimal _temperatureThreshold;
+
+        public SnowBot(bool enabled, string message, decimal temperatureThreshold) : base(enabled, message)
+        {
+            _temperatureThreshold = temperatureThreshold;
+        }
+
+        public override string? GetInfo(WeatherData weatherData)
+        {
+            if(_enabled && weatherData.Temperature <  _temperatureThreshold)
+                return _message; 
+            
+            return null;
+        }
+    }
+}
