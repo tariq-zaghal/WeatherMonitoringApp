126f4507a8dfeed14ab6e15e60e8fb38b42159e9
Tariq Zaghal <zaghaltariq007@gmail.com>
2023-09-21 18:54:50 +0300

    FEAT: Create class that contains the location of the Bot configerations file

---
+++ WeatherBotModels/BotConfigerationsFileLocation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherMonitoringApp.WeatherBotModels
+{
+    public class BotConfigerationsFileLocation
+    {
+        public static string BotConfigerationsFileAddress = @"C:\Users\TESTUSER\source\repos\WeatherMonitoringApp\WeatherMonitoringApp\BotConfigerations.json";
+    }
+}
