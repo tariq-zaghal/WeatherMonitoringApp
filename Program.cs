394b62975e9c33ba36e3f666f2e210da88b4340f
Tariq Zaghal <zaghaltariq007@gmail.com>
2023-09-21 19:16:31 +0300

    FEAT: Create Program class which controls the logic flow of the program

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using WeatherMonitoringApp.ConsoleLoggingModels;
+using WeatherMonitoringApp.WeatherBotModels;
+using WeatherMonitoringApp.WeatherDataFileModels;
+
+namespace WeatherMonitoringApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MainConsoleLogger.DisplayEnterFilePathMessage();
+            string filePath = ReadInputDataFilePath();
+
+            if (!VerifyFile.FileExits(filePath)) 
+            {
+                FileValidationLogger.DisplayFileInvalid();
+                return;
+            }
+
+            IWeatherDataFile? weatherDataFile = WeatherDataFileConsoleLogic.GetWeatherDataFile(filePath);
+
+            if (weatherDataFile == null)
+            {
+                FileValidationLogger.DisplayFileInvalidFormat();
+                return;
+            }
+
+            IWeatherData? weatherData = weatherDataFile.ConvertToWeatherData();
+
+            BotConfigerationsProvider botConfigerationsProvider = BotConfigerationsConsoleLogic.GetBotConfigerationsProvider();
+
+            List<string> notificationsList = WeatherNotifierConsoleLogic.CreateWeatherNotifications(botConfigerationsProvider, weatherData);
+
+            WeatherNotifierConsoleLogic.DisplayNotificationsToConsole(notificationsList);
+        }
+
+        private static string ReadInputDataFilePath()
+        {
+            return Console.ReadLine();
+        }
+    }
+}
