69c2b49abdde59c0453bfa09b5d7ee21c6290e3f
Tariq Zaghal <zaghaltariq007@gmail.com>
2023-09-21 18:47:53 +0300

    FEAT: Create data file factory which returns data file interface type

---
+++ WeatherDataFileModels/WeatherDataFileFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherMonitoringApp.WeatherDataFileModels
+{
+    public class WeatherDataFileFactory
+    {
+        public static IWeatherDataFile? GetDataFileInstance(string filePath, string fileContent)
+        {
+            if (DataFileFormatChecker.IsFileJson(filePath))
+                return new JsonWeatherDataFile(fileContent);
+            
+            if(DataFileFormatChecker.IsFileXml(filePath))
+                return new XmlWeatherDataFile(fileContent);
+
+            return null;
+        }
+    }
+}
