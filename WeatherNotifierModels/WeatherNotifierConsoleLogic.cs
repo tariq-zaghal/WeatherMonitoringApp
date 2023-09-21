4a745b3e82c91f8b1530b3b18d8c50768ad9abc4
Tariq Zaghal <zaghaltariq007@gmail.com>
2023-09-21 19:15:43 +0300

    FEAT: Create class that contains the logic related to the bots notification porcess in main

---
+++ WeatherNotifierModels/WeatherNotifierConsoleLogic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherMonitoringApp.WeatherBotModels;
+
+namespace WeatherMonitoringApp
+{
+    public class WeatherNotifierConsoleLogic
+    {
+        public static List<string> CreateWeatherNotifications(BotConfigerationsProvider botConfigerationsProvider, IWeatherData weatherData)
+        {
+            WeatherNotifier weatherNotifier = new WeatherNotifier(botConfigerationsProvider);
+
+            List<string> notificationsList = weatherNotifier.NotifyBots(weatherData);
+
+            return notificationsList;
+        }
+
+        public static void DisplayNotificationsToConsole(List<string> notificationsList)
+        {
+            notificationsList.ForEach(notification => Console.WriteLine(notification));
+        }
+    }
+}
