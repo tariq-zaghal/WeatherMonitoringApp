a3edd63fa6712d3860f77b367310f4dbb8edd42c
Tariq Zaghal <zaghaltariq007@gmail.com>
2023-09-21 18:38:05 +0300

    FEAT: Create xml data file implementation to deal with xml file inputs

---
+++ WeatherDataFileModels/XmlWeatherDataFile.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using WeatherMonitoringApp.WeatherDataModels;
+
+namespace WeatherMonitoringApp.WeatherDataFileModels
+{
+    public class XmlWeatherDataFile : IWeatherDataFile
+    {
+        private string _fileContent { get; set; }
+
+        internal XmlWeatherDataFile(string fileContent)
+        {
+            _fileContent = fileContent;
+        }
+
+        public IWeatherData? ConvertToWeatherData()
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(WeatherData));
+
+            WeatherData? output = null;
+
+            using (StringReader stringReader = new StringReader(_fileContent))
+            {
+                output = (WeatherData)serializer.Deserialize(stringReader);
+            }
+
+            return output;
+        }
+    }
+}
